e223d6204052ceaea7523f67caea831a05b8e251
MHRafix <fmhrafiz27@gmail.com>
2022-02-03 23:40:51 +0600

    code push

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
 /********************************************
  * Node server crud operation start from here
  ********************************************/
@@ -39,13 +38,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
             res.send(allProduct);
         });
 
-        // Get all products by id from the mongodb database
+        // Get all products and filter by category from the mongodb database
         app.get('/products/:category', async (req, res) => {
             const cat = req.params.category;
             const query = { category: cat };
             const findProducts = productsCollection.find(query);
             const result = await findProducts.toArray();
             res.send(result);
+        });
+
+        // Get all products and filter by size from the mongodb database
+        app.get('/products/sizedProducts/:size', async (req, res) => {
+            const findProducts = productsCollection.find({});
+            const allProducts = await findProducts.toArray();
+            const reqSize = req.params.size;
+            let sizesArr;
+            let matchedProducts = [];
+            for(const product of allProducts){
+                sizesArr = product.sizes;
+                if(sizesArr.includes(reqSize) === true){
+                    matchedProducts.push(product);
+                }else{
+                    //Nothing here
+                }
+            }
+            if(matchedProducts){
+                res.send(matchedProducts);
+            }
         });
     }
 
