7ea07c8992d6f2eb274f45a5b0be96556ed04c31
MHRafix <fmhrafiz27@gmail.com>
2022-02-22 17:37:08 +0600

    code push

--- index.js
+++ index.js
@@ -32,11 +32,19 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
         const productsCollection = database.collection('Products');
         const cartedProductsCollection = database.collection('CartedProducts');
         const wishListProductsCollection = database.collection('WishListProducts');
+        const allOrderedProductsCollection = database.collection('AllOrderedProducts');
 
         // Save the details of product to the database
         app.post('/addToCartList', async (req, res) => {
             const cartedProduct = req.body;
             const result = await cartedProductsCollection.insertOne(cartedProduct);
+            res.json(result);
+        });
+
+        // Save the details of customer and products to the database
+        app.post('/allCustomersOrders', async (req, res) => {
+            const ordersData = req.body;
+            const result = await allOrderedProductsCollection.insertOne(ordersData);
             res.json(result);
         });
 
